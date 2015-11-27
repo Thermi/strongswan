75701c45b39d9acb3e1bf1c412b58b4c5e5dcd29
lena jurkiewicz <lena.jurkiewicz@fancyfon.com>
2015-11-27 10:06:18 +0100

    code styling fixes
    Change-Id: Ida8f2d8d78e1a9cf61331dde76c583cdc93172f8

--- src/frontends/android/app/src/main/java/org/strongswan/android/ipc/verification/StrongswanCallerVerificator.java
+++ src/frontends/android/app/src/main/java/org/strongswan/android/ipc/verification/StrongswanCallerVerificator.java
@@ -26,54 +26,48 @@ public class StrongswanCallerVerificator implements CallerVerificator {
     private static final String TAG = "StrongswanCallerVerificator";
     static final String SHA_1 = "SHA-1";
     Context context;
-    String [] trustedFingerprints;
-    String [] trustedPackageNames;
+    String[] trustedFingerprints;
+    String[] trustedPackageNames;
 
 
     public StrongswanCallerVerificator(Context context) {
-         this.context=context;
-         trustedFingerprints=context.getResources().getStringArray(R.array.trusted_fingerprints);
-         trustedPackageNames=context.getResources().getStringArray(R.array.trusted_packages);
+        this.context = context;
+        trustedFingerprints = context.getResources().getStringArray(R.array.trusted_fingerprints);
+        trustedPackageNames = context.getResources().getStringArray(R.array.trusted_packages);
     }
 
 
-
     public boolean isCallerPermitted(int callingUid) {
-        String callingPackageName=context.getPackageManager().getNameForUid(callingUid);
-
-           String callerFingerprint=getCallerFingerprint(callingPackageName);
-             for (String trustedFingerprint  : trustedFingerprints ){
-                if(trustedFingerprint.equalsIgnoreCase(callerFingerprint)) {
-                    return verifyPackages(callingPackageName);
-                }
-              }
-
+        String callingPackageName = context.getPackageManager().getNameForUid(callingUid);
+        String callerFingerprint = getCallerFingerprint(callingPackageName);
+        for (String trustedFingerprint : trustedFingerprints) {
+            if (trustedFingerprint.equalsIgnoreCase(callerFingerprint)) {
+                return verifyPackages(callingPackageName);
+            }
+        }
         return false;
     }
 
     private boolean verifyPackages(String callingPackageName) {
-         return Arrays.asList(trustedPackageNames).contains(callingPackageName);
-
+        return Arrays.asList(trustedPackageNames).contains(callingPackageName);
     }
 
-
-    private String  getCallerFingerprint(String callingPackageName)   {
-        String fingerprint="";
-         try {
-            Signature[]   signatures = context.getPackageManager().getPackageInfo(callingPackageName,
-            PackageManager.GET_SIGNATURES).signatures;
-
-         for (Signature signature:signatures){
-            fingerprint =getFingerprint(signature);
-         }
+    private String getCallerFingerprint(String callingPackageName) {
+        String fingerprint = "";
+        try {
+            Signature[] signatures = context.getPackageManager().getPackageInfo(callingPackageName,
+                    PackageManager.GET_SIGNATURES).signatures;
+            for (Signature signature : signatures) {
+                fingerprint = getFingerprint(signature);
+            }
         } catch (PackageManager.NameNotFoundException e) {
-            Log.w(TAG,"Exception while determining caller signatures: "+e );
+            Log.w(TAG, "Exception while determining caller signatures: " + e);
         } catch (NoSuchAlgorithmException e) {
-             Log.w(TAG, "Error while calculation SHA1 from caller signature: "+e);
-         }
+            Log.w(TAG, "Error while calculating SHA1 from caller signature: " + e);
+        }
         return fingerprint;
-
     }
+
     private String getFingerprint(Signature signature) throws NoSuchAlgorithmException {
         String fingerprintSHA1;
         byte[] hexBytes = signature.toByteArray();
@@ -86,8 +80,6 @@ public class StrongswanCallerVerificator implements CallerVerificator {
         fingerprintSHA1 = sb.toString();
         return fingerprintSHA1;
     }
-
-
 
 
 }
