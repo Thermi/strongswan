7c7816aca3b45f149df97dcd16157c01da1c8157
Mariusz Rafalski <mariusz.rafalski@fancyfon.com>
2015-12-08 10:22:45 +0100

    fixing for deleteVpnProfile when profile already removed
    Change-Id: I1cbdcc07f25b17d95f640baac0d50281a4c54217

--- src/frontends/android/app/src/main/java/org/strongswan/android/data/VpnProfileDataSource.java
+++ src/frontends/android/app/src/main/java/org/strongswan/android/data/VpnProfileDataSource.java
@@ -219,7 +219,9 @@ public class VpnProfileDataSource
 	 * @return true if deleted, false otherwise
 	 */
 	public boolean deleteVpnProfile(VpnProfile profile)
-	{
+	{	if(profile == null) {
+			return true;
+		}
 		String name = profile.getName();
 		return mDatabase.delete(TABLE_VPNPROFILE, KEY_NAME + " = \"" + name + "\"", null) > 0;
 	}
