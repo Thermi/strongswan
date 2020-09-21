873f1910d7b07e0378b8360e01f875ca262613e7
Noel Kuntze <noel.kuntze@thermi.consulting>
2020-09-21 14:10:17 +0200

    Fix typo

--- src/libipsec/ipsec_processor.c
+++ src/libipsec/ipsec_processor.c
@@ -97,7 +97,6 @@ static job_requeue_t process_inbound(private_ipsec_processor_t *this)
 	uint32_t spi, reqid;
 	DBG0(DBG_LIB, "this->: %p", this);
 	DBG0(DBG_LIB, "this->inbound_queue: %p", this->inbound_queue);
-	DBG0(DBG_LIB, "this->inbound_queue->dequeu: %p", this->inbound_queue->dequeue);
 	DBG0(DBG_LIB, "this->inbound_queue->dequeue: %p", this->inbound_queue->dequeue);
 	packet = (esp_packet_t*)this->inbound_queue->dequeue(this->inbound_queue);
 
