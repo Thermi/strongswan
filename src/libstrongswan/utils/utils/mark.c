3f3638a3b1ad59b537aee738082f154d7d2cdfac
Noel Kuntze <noel.kuntze@thermi.consulting>
2022-08-04 02:55:53 +0200

    mark: fix

--- src/libstrongswan/utils/utils/mark.c
+++ src/libstrongswan/utils/utils/mark.c
@@ -74,25 +74,23 @@ METHOD(mark_tracker_t, get_mark, uint32_t, private_mark_tracker_t *this, uint32_
      * Get mark while respecting the mask
      */
     this->spinlock->lock(this->spinlock);
-    uint32_t next_value = this->mark_counter;
-    uint64_t cpy = next_value;
+    internal_mark_t *candidate = malloc(sizeof(internal_mark_t));
+    candidate->mark = this->mark_counter;
     while (true) {
-        next_value = increment_unmasked_bits(next_value, 1, mask);
-        cpy = next_value;
-        if (!this->hashtable->get(this->hashtable, (void *) cpy)) {
+        candidate->mark = increment_unmasked_bits(candidate->mark, 1, mask);
+        if (!this->hashtable->get(this->hashtable, candidate)) {
             break;
         }
-        if (cpy == this->mark_counter) {
+        if (candidate->mark == this->mark_counter) {
             /* For some reason this loop ran so long it wrapped around. So all marks are used. What do we do now?*/
             DBG0(DBG_CFG, "we ran out of marks to give out; starting again at 0. Mark reuse occurs now!");
         }
         
     }
-    this->mark_counter = next_value;
-    internal_mark_t *internal_mark = malloc(sizeof(internal_mark_t));
-    this->hashtable->put(this->hashtable, internal_mark, internal_mark);
+    this->mark_counter = candidate->mark;
+    this->hashtable->put(this->hashtable, candidate, candidate);
     this->spinlock->unlock(this->spinlock);
-    return next_value;
+    return candidate->mark;
 }
 
 u_int hash_mark(const void *key) {
